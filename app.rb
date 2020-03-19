62590808e7e91610e0ae63d526966d326959a6d3
Bucky Wolfe <igneous@usleep.consulting>
2020-03-20 00:26:26 +0700

    Add authors endpoint, and emit json (I heard that's good)!!

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'json'
 
 before do
   @quotes = File.read('quotes.txt')
@@ -9,9 +10,17 @@ before do
 end
 
 get '/quote' do
-  @quotes.sample
+  @quotes.sample.to_json
 end
 
 get '/quote/:num' do |quote_num|
-  @quotes[quote_num.to_i]
+  @quotes[quote_num.to_i].to_json
+end
+
+get '/authors' do
+  @quotes.map { |q| q.split('--').last }.sort.uniq.to_json
+end
+
+get '/authors/:num' do |author_num|
+  @quotes.map { |q| q.split('--').last }.sort.uniq[author_num].to_json
 end
